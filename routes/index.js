06a914543f81f8618ada08a23d1f8074fe4d4687
Danjelous <daniel.brunner@live.at>
2017-02-05 18:36:32 +0100

    Added Swagger JSDoc

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,115 @@ var router = express.Router();
 
 var db = require('../queries');
 
+/**  ----------------------------------------------
+ *   ----------- SWAGGER DEFINITIONS --------------
+ *   ---------------------------------------------* /
+/**
+ * @swagger
+ * definitions:
+ *   Article:
+ *     type: object
+ *     properties:
+ *       picture_path:
+ *         type: string
+ *         description: Path representing the image resource on the server.
+ *       name:
+ *         type: string
+ *         description: Name of the article.
+ *       description:
+ *         type: string
+ *         description: Description of the article.
+ *       price:
+ *         type: decimal
+ *         description: Price of the article in Euros. '9.99' for example.
+ */
+
+
+
+/**  ----------------------------------------------
+ *   ---------------- ENDPOINTS -------------------
+ *   ---------------------------------------------* /
+/**
+ * @swagger
+ * /api/articles:
+ *   get:
+ *     tags:
+ *       - Articles
+ *     description: Returns all articles
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of articles
+ *         schema:
+ *           $ref: '#/definitions/Article'
+ */
 router.get('/api/articles', db.getAllArticles);
+
+/**
+ * @swagger
+ * /api/articles/{id}:
+ *   get:
+ *     tags:
+ *       - Articles
+ *     description: Returns a single article by it's ID
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Article ID
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: A single article
+ *         schema:
+ *           $ref: '#/definitions/Article'
+ */
 router.get('/api/articles/:id', db.getArticleById);
+
+/**
+ * @swagger
+ * /api/articles:
+ *   post:
+ *     tags:
+ *       - Articles
+ *     description: Creates a new article
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: article
+ *         description: Article object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Article'
+ *     responses:
+ *       200:
+ *         description: Article successfully created
+ */
 router.post('/api/articles', db.createArticle);
+
+/**
+ * @swagger
+ * /api/articles/{id}:
+ *   delete:
+ *     tags:
+ *       - Articles
+ *     description: Deletes a single article
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Article ID
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: Article successfully deleted
+ */
 router.delete('/api/articles/:id', db.removeArticleById);
 
 module.exports = router;
