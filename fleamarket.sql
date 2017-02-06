70a3fc5c8957cf233a2488c8e8eaa89c38c3b5cc
Danjelous <daniel.brunner@live.at>
2017-02-06 16:49:19 +0100

    Added sample images and data

--- fleamarket.sql
+++ fleamarket.sql
@@ -11,5 +11,9 @@ CREATE TABLE articles (
   price DECIMAL
 );
 
-INSERT INTO articles (picture_path, name, description, price)
-  VALUES ('images/dummy.jpg', 'Demo', 'Demo desc', 9.99);
+INSERT INTO articles (picture_path, name, description, price) VALUES 
+  ('public/images/1.jpg', 'TV', 'Get back to your youth - instantly.', 99.99),
+  ('public/images/2.jpg', 'iHipster Phonemaster 2000', 'As the titleSpo suggests you can never miss a single call with this badboy.', 421.05),
+  ('public/images/3.jpg', 'Leica Retrospect', 'If you do not really like the future, just look at some pictures from the past. Good ol days.', 1010.01),
+  ('public/images/4.jpg', 'Garden Expander', '10/10 germans would buy.', 0.99),
+  ('public/images/5.jpg', 'iHipster Typerider', 'Typing like back then. Must buy if you own the Phonemaster 2000.', 812.12);
