89b9c37ff04a9c68800c84a88116c371c2dbbfc7
Danjelous <daniel.brunner@live.at>
2017-02-21 11:13:09 +0100

    Added Opbeat

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var swaggerJSDoc = require('swagger-jsdoc');
+var opbeat = require('opbeat').start();
 
 var routes = require('./routes/index');
 
